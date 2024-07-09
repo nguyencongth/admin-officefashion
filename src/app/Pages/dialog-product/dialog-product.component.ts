8f61da9dff726382df456c3455658c52ebb2a5e7
thanhnc <thanhnc@gemvietnam.com>
2024-07-09 17:36:50 +0700

    validator form product

--- src/app/Pages/dialog-product/dialog-product.component.ts
+++ src/app/Pages/dialog-product/dialog-product.component.ts
@@ -1,4 +1,4 @@
-import {Component, Inject, OnInit} from '@angular/core';
+import {Component, Inject, OnInit, ChangeDetectorRef} from '@angular/core';
 import {MatButtonModule} from '@angular/material/button';
 import {MAT_DIALOG_DATA, MatDialogModule, MatDialogRef} from '@angular/material/dialog';
 import {Validators, ReactiveFormsModule, FormBuilder} from '@angular/forms';
@@ -28,23 +28,30 @@ import {ProductService} from "../../core/service/product.service";
 })
 export class DialogProductComponent implements OnInit {
   categories: any[] = [];
+  imageUrl: string;
   constructor(
     @Inject(MAT_DIALOG_DATA) public data: any,
     public dialogRef: MatDialogRef<DialogProductComponent>,
     private fb: FormBuilder,
     private categoryService: CategoryService,
     private productService: ProductService,
+    private cdRef: ChangeDetectorRef
   ) {}
   ngOnInit(): void {
     this.getCategory();
+    this.imageUrl = this.formProduct.get('imageProduct')?.value;
+    this.formProduct.get('imageProduct')?.valueChanges.subscribe((value) => {
+      this.imageUrl = value;
+      this.cdRef.detectChanges();
+    });
   }
   formProduct = this.fb.group({
     categoryId: [this.data.categoryId, Validators.required],
     productName: [this.data.productName, Validators.required],
-    entryPrice: [this.data.entryPrice, Validators.required],
-    price: [this.data.price, Validators.required],
-    quantityStock: [this.data.quantityStock, Validators.required],
-    quantitySold: [this.data.quantitySold, Validators.required],
+    entryPrice: [this.data.entryPrice, [Validators.required, Validators.min(0)]],
+    price: [this.data.price, [Validators.required, Validators.min(0)]],
+    quantityStock: [this.data.quantityStock, [Validators.required, Validators.min(0)]],
+    quantitySold: [this.data.quantitySold, [Validators.required, Validators.min(0)]],
     imageProduct: [this.data.imageProduct, Validators.required],
     descProduct: [this.data.descProduct, Validators.required],
     dateAdded: [this.data.isAdd ? "2024-07-09T07:35:26.604Z" : this.data.dateAdded, Validators.required],
