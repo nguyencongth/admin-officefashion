925f149a6fb75077b9bab73296dcbad1f2ff34c6
thanhnc <thanhcong27092002@gmail.com>
2024-07-18 23:36:34 +0700

    Export invoice in PDF

--- src/app/Pages/order-list/dialog-order-detail/dialog-order-detail.component.ts
+++ src/app/Pages/order-list/dialog-order-detail/dialog-order-detail.component.ts
@@ -3,6 +3,9 @@ import {MAT_DIALOG_DATA, MatDialogModule} from "@angular/material/dialog";
 import {CurrencyFormatPipe} from "../../../core/pipes/currency-format.pipe";
 import {DatetimeFormatPipe} from "../../../core/pipes/datetime-format.pipe";
 import {NgFor} from "@angular/common";
+import {MatButton} from "@angular/material/button";
+import { jsPDF } from 'jspdf';
+import html2canvas from 'html2canvas';
 
 @Component({
   selector: 'app-dialog-order-detail',
@@ -11,7 +14,8 @@ import {NgFor} from "@angular/common";
     MatDialogModule,
     CurrencyFormatPipe,
     DatetimeFormatPipe,
-    NgFor
+    NgFor,
+    MatButton
   ],
   templateUrl: './dialog-order-detail.component.html',
   styleUrl: './dialog-order-detail.component.css'
@@ -22,5 +26,19 @@ export class DialogOrderDetailComponent implements OnInit {
   ) {}
   ngOnInit() {
     console.log(this.data);
+  }
+
+  generatePDF(id: number) {
+    const DATA: any = document.getElementById('invoice');
+    html2canvas(DATA).then((canvas) => {
+      const fileWidth = 210;
+      const fileHeight = (canvas.height * fileWidth) / canvas.width;
+      const FILEURI = canvas.toDataURL('image/png');
+      const PDF = new jsPDF('p', 'mm', 'a4');
+      const position = 0;
+      PDF.addImage(FILEURI, 'PNG', 0, position, fileWidth, fileHeight);
+      const fileName = `invoice_#${id}.pdf`;
+      PDF.save(fileName);
+    });
   }
 }
