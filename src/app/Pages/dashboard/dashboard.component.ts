2f14a398cab30e805fa013e312b70433efd080ee
thanhnc <thanhcong27092002@gmail.com>
2024-07-24 23:39:25 +0700

    UI dashboard

--- src/app/Pages/dashboard/dashboard.component.ts
+++ src/app/Pages/dashboard/dashboard.component.ts
@@ -2,13 +2,15 @@ import { Component } from '@angular/core';
 import {
   Top5ProductBestSellingComponent
 } from "../statistical/top5-product-best-selling/top5-product-best-selling.component";
+import {MatIcon} from "@angular/material/icon";
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [
-    Top5ProductBestSellingComponent
-  ],
+    imports: [
+        Top5ProductBestSellingComponent,
+        MatIcon
+    ],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.css'
 })
